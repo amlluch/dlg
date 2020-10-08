c3ab03077025e0b803a1121564702897caf535c4
Alfonso <amlluch@gmail.com>
2020-10-08 20:50:33 +0100

    optimize imports

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
+from typing import Tuple
 
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
@@ -8,24 +9,24 @@ numbers_to_add = list(range(10000001))
 
 
 @app.route('/total', methods=['GET'])
-def total() -> str:
+def total() -> Tuple[Response, int]:
 
     # prioritize query string as url can be cached
     try:
         list_of_numbers = json.loads(request.args.get("list"))
     except TypeError:
         if not request.data:
-            return make_response('Invalid input. No input data.', 400)
+            return Response('Invalid input. No input data.'), 400
         list_of_numbers = request.get_json(force=True)
 
     if type(list_of_numbers) is not list:
-        return make_response('Invalid input. It must be a list', 400)
+        return Response('Invalid input. It must be a list'), 400
     if not len(list_of_numbers):
-        return make_response('Invalid input. Empty list.', 400)
+        return Response('Invalid input. Empty list.'), 400
     if not set(list_of_numbers).issubset(numbers_to_add):
-        return make_response('Some value(s) in the list out of range', 416)
+        return Response('Some value(s) in the list out of range'), 416
 
-    return jsonify({"total": sum(list_of_numbers)})
+    return jsonify({"total": sum(list_of_numbers)}), 200
 
 
 if __name__ == '__main__':
