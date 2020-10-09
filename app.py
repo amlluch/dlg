0a452fe2a03931073718f2b7d9272434def22d45
Alfonso <amlluch@gmail.com>
2020-10-09 12:01:14 +0100

    black format added

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 numbers_to_add = list(range(10000001))
 
 
-@app.route('/total', methods=['GET'])
+@app.route("/total", methods=["GET"])
 def total() -> Tuple[Response, int]:
 
     # prioritize query string as url can be cached
@@ -16,18 +16,18 @@ def total() -> Tuple[Response, int]:
         list_of_numbers = json.loads(request.args.get("list"))
     except TypeError:
         if not request.data:
-            return Response('Invalid input. No input data.'), 400
+            return Response("Invalid input. No input data."), 400
         list_of_numbers = request.get_json(force=True)
 
     if type(list_of_numbers) is not list:
-        return Response('Invalid input. It must be a list'), 400
+        return Response("Invalid input. It must be a list"), 400
     if not len(list_of_numbers):
-        return Response('Invalid input. Empty list.'), 400
+        return Response("Invalid input. Empty list."), 400
     if not set(list_of_numbers).issubset(numbers_to_add):
-        return Response('Some value(s) in the list out of range'), 416
+        return Response("Some value(s) in the list out of range"), 416
 
     return jsonify({"total": sum(list_of_numbers)}), 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,6 @@ from app import app
 
 
 class TestDlg(flask_testing.TestCase):
-
     def setUp(self) -> None:
         ...
 
@@ -19,7 +18,7 @@ class TestDlg(flask_testing.TestCase):
     def test_list_in_range_in_query_string(self) -> None:
         list_of_numbers = str([1, 2, 3])
         with app.test_client() as c:
-            resp = c.get(f'/total?list={list_of_numbers}')
+            resp = c.get(f"/total?list={list_of_numbers}")
         self.assertEqual(resp.status_code, 200)
         json_data = resp.get_json()
         self.assertEqual(json_data["total"], 6)
@@ -27,41 +26,41 @@ class TestDlg(flask_testing.TestCase):
     def test_empty_list(self) -> None:
         empty_list = str([])
         with app.test_client() as c:
-            resp_querystring = c.get(f'/total?list={empty_list}')
-            resp_body_param = c.get('/total', data=empty_list)
+            resp_querystring = c.get(f"/total?list={empty_list}")
+            resp_body_param = c.get("/total", data=empty_list)
         self.assertEqual(resp_querystring.status_code, 400)
-        self.assertEqual(resp_querystring.data, b'Invalid input. Empty list.')
+        self.assertEqual(resp_querystring.data, b"Invalid input. Empty list.")
         self.assertEqual(resp_body_param.status_code, 400)
-        self.assertEqual(resp_body_param.data, b'Invalid input. Empty list.')
+        self.assertEqual(resp_body_param.data, b"Invalid input. Empty list.")
 
     def test_big_list(self) -> None:
         big_list = str(list(range(10000001)))
         with app.test_client() as c:
-            resp_querystring = c.get(f'/total?list={big_list}')
-            resp_body_param = c.get('/total', data=big_list)
+            resp_querystring = c.get(f"/total?list={big_list}")
+            resp_body_param = c.get("/total", data=big_list)
         self.assertEqual(resp_querystring.status_code, 200)
         self.assertEqual(resp_body_param.status_code, 200)
 
     def test_big_list_out_of_range(self) -> None:
         big_list = str(list(range(10000002)))
         with app.test_client() as c:
-            resp_querystring = c.get(f'/total?list={big_list}')
-            resp_body_param = c.get('/total', data=big_list)
+            resp_querystring = c.get(f"/total?list={big_list}")
+            resp_body_param = c.get("/total", data=big_list)
         self.assertEqual(resp_querystring.status_code, 416)
         self.assertEqual(resp_body_param.status_code, 416)
 
     def test_list_out_range(self) -> None:
         all_integers = str([0, -1, 2])
         with app.test_client() as c:
-            resp_querystring = c.get(f'/total?list={all_integers}')
-            resp_body_param = c.get('/total', data=all_integers)
+            resp_querystring = c.get(f"/total?list={all_integers}")
+            resp_body_param = c.get("/total", data=all_integers)
         self.assertEqual(resp_querystring.status_code, 416)
         self.assertEqual(resp_body_param.status_code, 416)
 
     def test_list_in_range_in_parameter(self) -> None:
         list_of_numbers = str([1, 2, 3, 4])
         with app.test_client() as c:
-            resp = c.get('/total', data=list_of_numbers)
+            resp = c.get("/total", data=list_of_numbers)
         self.assertEqual(resp.status_code, 200)
         json_data = resp.get_json()
         self.assertEqual(json_data["total"], 10)
@@ -75,7 +74,7 @@ class TestDlg(flask_testing.TestCase):
         )
         for parameters in fixtures:
             with app.test_client() as c:
-                resp = c.get(f'/total?list={parameters[0]}', data=parameters[1])
+                resp = c.get(f"/total?list={parameters[0]}", data=parameters[1])
             self.assertEqual(resp.status_code, parameters[2])
             if parameters[2] == 200:
                 json_data = resp.get_json()
@@ -83,7 +82,7 @@ class TestDlg(flask_testing.TestCase):
 
     def test_no_data(self) -> None:
         with app.test_client() as c:
-            resp = c.get('/total')
+            resp = c.get("/total")
         self.assertEqual(resp.status_code, 400)
 
 
